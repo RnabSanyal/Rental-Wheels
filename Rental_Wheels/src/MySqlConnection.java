71c0c45c323b94aaf5350a5127a99a08b227b95e
RnabSanyal <arnab.sayla902@gmail.com>
2017-09-05 10:15:28 +0530

    Completed

---
+++ Rental_Wheels/src/MySqlConnection.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RNV
+ */
+import java.sql.DriverManager;
+import java.sql.*;
+import javax.swing.*;
+
+public class MySqlConnection {
+    
+    Connection conn = null;
+    public static Connection ConnectDb()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/car_rental","root","");
+            JOptionPane.showMessageDialog(null,"Connection Established");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+    
+    
+}
